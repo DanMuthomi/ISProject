434b8ed757014e49d897d5b906828a902d5ab92a
Daniel Muthomi <thedanielmuthomi@gmail.com>
2023-07-23 21:11:09 +0300

    added login page

---
+++ Home/login/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="form.css">
+</head>
+<body>
+	<h1>LOGIN</h1>
+	<form action="login.php" method="post">
+		<div class="login">
+		
+		<label>Username</label>
+		<input type="text" name="uname" placeholder="User name">
+		<br>
+		<label>Password</label>
+		<input type="password" name="password" placeholder="Password">
+		<br>
+		<button type="submit">Login</button>
+	</div>
+	</form>
+</body>
+</html>
